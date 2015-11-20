4bfa5f808b0cca3022049fb89e98b3ba9c03f6d1
xiao_dli <ietjxdl@gmail.com>
2015-11-20 16:40:13 +0800

    use ARGV.empty? replace ARGV.length == 0

--- lib/bone_hola/translator.rb
+++ lib/bone_hola/translator.rb
@@ -1,22 +1,24 @@
 # translater content by language
-class BoneHola::Translator
-  attr_accessor :language
-  private :language, :language=
+module BoneHola
+  class Translator
+    attr_accessor :language
+    private :language, :language=
 
-  # init say language
-  def initialize(language)
-    self.language = language
-  end
+    # init say language
+    def initialize(language)
+      self.language = language
+    end
 
-  # Say hi to the world through tree languanges
-  def hi
-    case language
-      when "english"
-        "Hello World!"
-      when "spanish"
-        "hola mundo!"
-      else
-        "你好，世界!"
+    # Say hi to the world through tree languanges
+    def hi
+      case language
+        when "english"
+          "Hello World!"
+        when "spanish"
+          "hola mundo!"
+        else
+          "你好，世界!"
+      end
     end
   end
 end
